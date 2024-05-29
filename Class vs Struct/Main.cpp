1eff4c72f477a261cdac6d745c83abfe6084b970
Kulpot <kulpotwahu@gmail.com>
2024-05-30 02:11:16 +0800

    class Vector3 {  double x;  double y;  double z;

--- Class vs Struct/Main.cpp
+++ Class vs Struct/Main.cpp
@@ -3,5 +3,7 @@
 //Class vs Struct | C++ Tutorials	//ref link:https://www.youtube.com/watch?v=FnwbsHtCJ1o&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=3
 
 class Vector3 {
-
+	double x;
+	double y;
+	double z;
 };
