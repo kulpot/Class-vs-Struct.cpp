b5583bf747144b6c85008053091017a2a64f7e3e
Kulpot <kulpotwahu@gmail.com>
2024-05-31 18:59:38 +0800

    //Class - private by default, Struct - public by default

--- Class vs Struct/Main.cpp
+++ Class vs Struct/Main.cpp
@@ -1,6 +1,7 @@
 
 
 //Class vs Struct | C++ Tutorials	//ref link:https://www.youtube.com/watch?v=FnwbsHtCJ1o&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=3
+//Class - private by default, Struct - public by default
 
 struct Vector3 {
 //class Vector3 {
