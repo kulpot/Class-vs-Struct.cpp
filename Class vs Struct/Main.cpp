cb0532ee9ae39770b1eb50fcac29ae6cf5f28f2e
Kulpot <kulpotwahu@gmail.com>
2024-05-31 19:00:08 +0800

    ..

--- Class vs Struct/Main.cpp
+++ Class vs Struct/Main.cpp
@@ -1,5 +1,4 @@
 
-
 //Class vs Struct | C++ Tutorials	//ref link:https://www.youtube.com/watch?v=FnwbsHtCJ1o&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=3
 //Class - private by default, Struct - public by default
 
